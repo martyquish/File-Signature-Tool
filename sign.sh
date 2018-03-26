1954eec867202e64581ea47603726916689b44e7
Marty Quish <marty.quish@gmail.com>
2018-03-26 17:37:18 -0500

    Removed issue where script would print the tildes (~) in signatures.txt into files without matched extensions

--- sign.sh
+++ sign.sh
@@ -21,7 +21,7 @@ for file in "$@";do
 		# a // is automatically prepended to each line of the signature before insertion
 		
 		cat $file > tempsigbody.txt
-		sed -e "s/^\s\?/\/\/ /g" -e "s/^\s\?\/\/\s\?[~]\s\?//g"   signature.txt > tempmodsig.txt
+		sed -e "s/^\s\?/\/\/ /g" -e "s/^\s\?\/\/\s\?[~]\s\?//g" signature.txt > tempmodsig.txt
 		eval cat tempmodsig.txt tempsigbody.txt > "$file"
 		rm tempsigbody.txt tempmodsig.txt
 		
@@ -31,7 +31,7 @@ for file in "$@";do
 		# a ; is automatically prepended to each line of the signature before insertion
 		
 		cat $file > tempsigbody.txt
-		sed -e "s/^\s\?/\; /g" -e "s/^\s\?\;\s\?[~]\s\?//g"   signature.txt > tempmodsig.txt
+		sed -e "s/^\s\?/\; /g" -e "s/^\s\?\;\s\?[~]\s\?//g" signature.txt > tempmodsig.txt
 		eval cat tempmodsig.txt tempsigbody.txt > "$file"
 		rm tempsigbody.txt tempmodsig.txt
 		
@@ -39,26 +39,29 @@ for file in "$@";do
 	    then
 		# If the file is found to fit a format which typically uses '#' to indicate comments,
 		# a # is automatically prepended to each line of the signature before insertion
-	     	sed -e "s/^\s\?/\# /g" -e "s/^\s\?\#\s\?[~]\s\?//g"   signature.txt > tempmodsig.txt
+		
+	       	sed -e "s/^\s\?/\# /g" -e "s/^\s\?\#\s\?[~]\s\?//g" signature.txt > tempmodsig.txt 
 		cat $file > tempsigbody.txt
-
 		eval cat tempmodsig.txt tempsigbody.txt > "$file"
 		rm tempsigbody.txt tempmodsig.txt
 		
 	    else
-		# If no outstanding file types are detected, prepend signature without modification.
+		# If no outstanding file types are detected, remove leading '~' flags and prepend signature as-is.
 		
+		sed -e "s/^\s\?[~]\s\?//g" signature.txt > tempmodsig.txt
 		cat $file > tempsigbody.txt
-		eval cat signature.txt tempsigbody.txt > "$file"
-		rm tempsigbody.txt
+		eval cat tempmodsig.txt tempsigbody.txt > "$file"
+		rm tempsigbody.txt tempmodsig.txt
 		
 	    fi
 	else
+	    # The user has not created a signature yet.
 	    echo "You have not created a signature yet! Please write a signature in the current directory into a file named 'signature.txt'. "
 	    echo "This script will automatically attempt to format the signature as a comment in the language of your file. "
 	    echo "Files without recognizable extensions will have the signature prepended in plaintext"
 	fi
     else
+	# The current file does not exist.
 	echo "Could not find file: $file"
     fi
 done
